19e9c02bbb6c24292846eb754d1f8fe92ee47f45
Bernard Mattéo <matteo_bernard1@ens.univ-artois.fr>
2024-05-17 19:28:45 +0200

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import './App.css';
+import './output.css';
 import {Route, Routes} from "react-router-dom";
 import {Dashboard, Login, Playlists, RecentlyPlayed, ShowPlaylist, TopArtists, TopTracks} from "./pages";
 
 function App() {
   return (
-      <Routes>
-        <Route path={"/"} element={<Login />}/>
-        <Route path={"/dashboard"} element={<Dashboard />}/>
-        <Route path={"/playlists"} element={<Playlists />}/>
-        <Route path={"/showPlaylist/:playlistId"} element={<ShowPlaylist />}/>
-        <Route path={"/topTracks"} element={<TopTracks />}/>
-        <Route path={"/topArtists"} element={<TopArtists />}/>
-        <Route path={"/recentlyPlayed"} element={<RecentlyPlayed />}/>
-      </Routes>
-  )
+      <div className={"App"}>
+          <Routes>
+              <Route path={"/"} element={<Login />}/>
+              <Route path={"/dashboard"} element={<Dashboard />}/>
+              <Route path={"/playlists"} element={<Playlists />}/>
+              <Route path={"/showPlaylist/:playlistId"} element={<ShowPlaylist />}/>
+              <Route path={"/topTracks"} element={<TopTracks />}/>
+              <Route path={"/topArtists"} element={<TopArtists />}/>
+              <Route path={"/recentlyPlayed"} element={<RecentlyPlayed />}/>
+          </Routes>
+      </div>
+  );
 }
 
 export default App;
